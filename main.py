1f2ca98c6409642840ab7b23ff518c7afe35c809
saden0789-hue <saden0789@gmail.com>
2026-09-18 08:46:13 -0700

    Create  main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+name = "سدين الشماسين"
+major = "ذكاء الأعمال وتحليل البيانات"
+university = "جامعة البلقاء التطبيقية - كلية الكرك"
+graduation_year = 2026
+hobby = "تحليل البيانات"
+
+print("Student Profile")
+print("----------------")
+print("Name:", name)
+print("Major:", major)
+print("University:", university)
+print("Graduation Year:", graduation_year)
+print("Hobby:", hobby)
